bb1bb8c9bb4bfb8928bc4e9019538a38e2a6f5be
Gleb Siroki <5238128+Trave11er@users.noreply.github.com>
2021-06-26 22:25:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Tight-binding model for topological nanoparticles [ARCHIVED]
+
+**DISCLAIMER** This is bad code kept only for reproducibility reasons (see below)
+
+- Microscopic tight-binding model applied to topological insulator nanoparticles as in:
+
+  - G. Siroki, P. D. Haynes, D. K. K. Lee and V. Giannini Phys. Rev. Mater. 1, 024201 (2017)
+
+- The original tight-binding Hamiltonian is presented here:
+
+   -  L. Fu, C. L. Kane, and E. J. Mele, Phys. Rev. Lett. 98, 106803 (2007)
+
+Spherical, Cubic, Rhombohedral and Spherical Shell nanoparticles are supported. These inherit common properties
+from the Particle class. An example input.txt file is provided. To compile run 'make'.
+
+## Getting Started
+To compile type
+```
+make
+```
+To run execute
+```
+./main
+```
+which will read the file input.txt (example provided) for particle's shape and size and run the calculation.
+The output files contain the eigenvalues and probability density of the associated wavefunctions.
+### Dependencies
+
+Armadillo, LAPACK, BLAS
+
+## Authors
+
+* **Gleb Siroki**
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ main.cc
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <armadillo>
+#include<complex>
+#include<cstdlib>
+#include <ctime>
+#include <string>
+#include "Particle.h"
+#include "Spherical.h"
+#include "Cubic.h"
+#include "Rhombohedral.h"
+#include "Shell.h"
+using namespace std;                                                                                 
+using namespace arma;                                                                                
+
+int main(){
+    // Input 
+    int sizeCells;  //sizeCells of lattice in +/-ve x/y/z direction
+    int pSizeCells;  // size of the particle (radius, edge) in unit cells - carved out of bulk lattice
+    double disorderStrength; // Magnitude of disorder potential
+    double disorderCoverage; // fraction of disordered states above orderRadius
+    char pShape; // Spherical, Cubic or Rhombohedral
+
+
+    // Parameters and data structures (Don't need to change those)
+    const double tHopping = 1;
+    const double deltatHopping = 0.4; 
+    const complex<double> spinOrbitCoupling(0,1); 
+
+    // Geometry of the model;
+    const rowvec latVec1 ("0 0.5 0.5");
+    const rowvec latVec2 ("0.5 0 0.5");
+    const rowvec latVec3 ("0.5 0.5 0");
+    const rowvec sublatVec ("0.25 0.25 0.25");
+    const rowvec comShift ("-0.5 -0.5 -0.5"); // to have centre of mass of sublatOne at the origin
+    const double latVecNorm = 1/sqrt(2); // cubic cell size a = 1
+    const double sublatVecNorm = sqrt(3)/4;
+
+    const double delta = 0.01; // small number for numerical comparisons
+
+    // Read input from file
+    ifstream inputfile;
+    inputfile.open("input.txt");
+    char buffer [512];
+    string line;
+    while (getline(inputfile, line)) {
+        sscanf(line.c_str(), "sizeCells = %d %s", &sizeCells, buffer);
+        sscanf(line.c_str(), "particleSizeCells = %d %s", &pSizeCells, buffer);
+        sscanf(line.c_str(), "disorderStrength = %lf %s", &disorderStrength, buffer);
+        sscanf(line.c_str(), "disorderCoverage = %lf %s", &disorderCoverage, buffer);
+        sscanf(line.c_str(), "particleShape = %c %s", &pShape, buffer);
+    }
+
+    Particle * myParticle;
+
+    if (pShape == 'S' || pShape == 's') {
+        myParticle = new Spherical(pSizeCells*latVecNorm, pSizeCells*latVecNorm - sublatVecNorm);
+    } else if (pShape == 'C' ||  pShape == 'c') {
+        myParticle = new Cubic (pSizeCells*latVecNorm, pSizeCells*latVecNorm - sublatVecNorm);
+    } else if (pShape == 'R' || pShape == 'r') {
+        myParticle = new Rhombohedral(pSizeCells*latVecNorm, pSizeCells*latVecNorm - sublatVecNorm); 
+	} else if (pShape == 'H' || pShape == 'h') {
+        myParticle = new Shell(pSizeCells*latVecNorm, pSizeCells*latVecNorm - 4*sublatVecNorm);
+    } else { cout << "Wrong particle type - exiting" << endl; return 1;}
+    cout << "Input: "<< sizeCells << " " << pSizeCells << " " <<  disorderStrength << " " << disorderCoverage << " " << myParticle->shape << endl;
+
+    int pCells = 0; // # unit cells belonging to the particle
+    int dummy = 0; 
+    rowvec dummyVec(3);
+    cx_mat dummyMat(2,2);
+    rowvec pcomShift(3);
+
+    cx_mat unity(2,2), sigma_x(2,2), sigma_y(2,2), sigma_z(2,2); // Pauli matrices
+    unity.fill(0), sigma_x.fill(0), sigma_y.fill(0), sigma_z.fill(0);
+    mat dummy_unity={{1,0},{0,1}}, dummy_sigma_x={{0,1},{1,0}};
+    mat dummy_sigma_y={{0,-1},{1,0}}, dummy_sigma_z={{1,0},{0,-1}};
+    unity.set_real(dummy_unity);
+    sigma_x.set_real(dummy_sigma_x);
+    sigma_y.set_imag(dummy_sigma_y);
+    sigma_z.set_real(dummy_sigma_z);
+
+    // Rhombohedral has the shape of the unit cell, so don't need to carve out
+    if (myParticle->shape == "Rhombohedral") {
+        sizeCells = pSizeCells; 
+    } 
+
+    // Set up lattice and calculate number of cells within particle
+    for (int i=-sizeCells; i<sizeCells; i++) {
+        for (int j=-sizeCells;  j<sizeCells; j++) {
+            for (int k=-sizeCells; k<sizeCells; k++) {
+                dummyVec=i*latVec1+j*latVec2+k*latVec3-comShift;
+                if (myParticle->WithinParticle(dummyVec)) {
+                    pcomShift=pcomShift+2*dummyVec+sublatVec;
+                    pCells++;
+                }
+            }
+        }
+    }   
+
+    // Finding particle's COM
+    pcomShift=0.5*pcomShift/pCells;
+
+    // Set up data structures for the particle
+    myParticle->SetDataStructures(pCells);
+
+    // Carve out the unit sphere out of lattice
+    for (int i=-sizeCells; i<sizeCells; i++) {
+        for (int j=-sizeCells;  j<sizeCells; j++) {
+            for (int k=-sizeCells; k<sizeCells; k++) {
+                dummyVec=i*latVec1+j*latVec2+k*latVec3-comShift;
+                if (myParticle->WithinParticle(dummyVec)) {
+                    myParticle->sublatOne(dummy,0)=dummy;
+                    myParticle->sublatOne(dummy,span(1,3))=dummyVec-pcomShift; 
+                    myParticle->sublatTwo(dummy,0)=dummy;
+                    myParticle->sublatTwo(dummy,span(1,3))=dummyVec+sublatVec-pcomShift;
+                    dummy++;
+                }
+            }
+        }
+    }   
+
+    // Set up the Hamiltonian
+    for (int i=0; i<pCells; i++) {
+        for (int j=0; j<pCells; j++) {
+            // Set up nearest neighbour hoppings (between sublattices)
+            dummyVec=myParticle->sublatOne(i,span(1,3))-myParticle->sublatTwo(j,span(1,3));
+            if (dot(dummyVec,dummyVec) < sublatVecNorm*sublatVecNorm+delta) {
+                if (i==j) {
+                    // hoppings within a cell i
+                    myParticle->Hamiltonian(span(4*i+0,4*i+1),span(4*i+2,4*i+3))+=(tHopping+deltatHopping)*unity;
+                    myParticle->Hamiltonian(span(4*i+2,4*i+3),span(4*i+0,4*i+1))+=(tHopping+deltatHopping)*unity;
+                } else {
+                    // hoppings from sublatTwo of cell j to sublatOne of cell i
+                    myParticle->Hamiltonian(span(4*i+0,4*i+1),span(4*j+2,4*j+3))+=(tHopping)*unity;
+                    // hoppings from sublatOne of cell i to sublatTwo of cell j
+                    myParticle->Hamiltonian(span(4*j+2,4*j+3),span(4*i+0,4*i+1))+=(tHopping)*unity;
+                }
+            }
+
+            // Set up next nearest neighbour hoppings (within each sublattice)
+            dummyVec=myParticle->sublatOne(i,span(1,3))-myParticle->sublatOne(j,span(1,3)); 
+            if ( (dot(dummyVec,dummyVec) < latVecNorm*latVecNorm+delta)  && 
+                    (dot(dummyVec,dummyVec) > sublatVecNorm*sublatVecNorm+delta) ) { 
+
+                // Matrix elements are direction-dependent
+                if ( (dummyVec(0) > 0 && dummyVec(1) > 0) || (dummyVec(0) > 0 &&
+                            dummyVec(2) > 0) || (dummyVec(1) > 0 && dummyVec(2) > 0) ) {
+                    dummyVec = cross(sublatVec, dummyVec-sublatVec);
+                } else if ( (dummyVec(1) < 0 && dummyVec(2) < 0) || (dummyVec(0) > 0 &&
+                            dummyVec(1) < 0) || (dummyVec(0) > 0 && dummyVec(2) < 0) ) {
+                    dummyVec = cross(-latVec1+sublatVec, dummyVec+latVec1-sublatVec);
+                } else if ( (dummyVec(0) < 0 && dummyVec(1) > 0) || (dummyVec(0) < 0 &&
+                            dummyVec(2) < 0) || (dummyVec(1) > 0 && dummyVec(2) < 0) ) {
+                    dummyVec = cross(-latVec2+sublatVec, dummyVec+latVec2-sublatVec);
+                } else if ( (dummyVec(0) < 0 && dummyVec(2) > 0) || (dummyVec(1) < 0 &&
+                            dummyVec(2) > 0) || (dummyVec(0) < 0 && dummyVec(1) < 0) ) {
+                    dummyVec = cross(-latVec3+sublatVec, dummyVec+latVec3-sublatVec);
+                } else {}
+                dummyMat=dummyVec(0)*sigma_x+dummyVec(1)*sigma_y+dummyVec(2)*sigma_z;
+                // See if both atoms are connected to the same linking atom
+                // hoppings from cell j to cell i within sublattice One
+                myParticle->Hamiltonian(span(4*i,4*i+1),span(4*j,4*j+1))=spinOrbitCoupling*dummyMat;
+                // hoppings from cell i to cell j within sublattice One
+                // Minus sign for hopping in opposite direction
+                myParticle->Hamiltonian(span(4*j,4*j+1),span(4*i,4*i+1))=-spinOrbitCoupling*dummyMat;
+                // hoppings from cell j to cell i within sublattice Two
+                // Minus sign for sublattice  
+                myParticle->Hamiltonian(span(4*i+2,4*i+3),span(4*j+2,4*j+3))=-spinOrbitCoupling*dummyMat;
+                // hoppings from cell i to cell j within sublattice Two
+                myParticle->Hamiltonian(span(4*j+2,4*j+3),span(4*i+2,4*i+3))=spinOrbitCoupling*dummyMat;
+            }
+        } 
+    }
+
+    myParticle->AddDisorder(disorderStrength, disorderCoverage, delta);
+
+    // abs returns a matrix, max returns a vector, 2nd max - largest value
+    if (max(max(abs(myParticle->Hamiltonian-myParticle->Hamiltonian.t()))) > delta ) {
+        cout << "Error, Hamiltonian is not Hermitian by at least " << delta << endl;
+    }
+
+    // Get the eigenvectors and eigevalues
+    eig_sym(myParticle->eigvals, myParticle->eigvecs, myParticle->Hamiltonian);
+
+    myParticle->PrintInfo(disorderStrength);
+
+    // Find the middle of the spectrum (Dirac point, E=0, for clean spectrum) 
+    for (int i=0; i<4*pCells-1;i++) {
+        if (myParticle->eigvals(i) > 0 && myParticle->eigvals(i-1) < 0) {
+            dummy = i ;
+        }
+    }
+    cout << "First state above E=0 is #: " << dummy << " with E= " << myParticle->eigvals(dummy) << endl; 
+    dummy=2*pCells;
+    cout << "Picking state #: " << dummy << " with E= " << myParticle->eigvals(dummy) << endl; 
+
+    mat probDensity(2*pCells,4+100); 
+    probDensity(span(0,pCells-1),span(0,2))=myParticle->sublatOne.cols(1,3);
+    probDensity(span(pCells,2*pCells-1),span(0,2))=myParticle->sublatTwo.cols(1,3);
+
+    // Calculate probability density of probDensity labelled dummy at each lattice site
+    for (int i=0; i<pCells; i++) {
+		for (int j=0; j<100; j++) {
+        probDensity(i,3+j)=norm(myParticle->eigvecs(span(4*i,4*i+1),dummy+j))*norm(myParticle->eigvecs(span(4*i,4*i+1),dummy+j));
+        probDensity(pCells+i,3+j)=norm(myParticle->eigvecs(span(4*i+2,4*i+3),dummy+j))*norm(myParticle->eigvecs(span(4*i+2,4*i+3),dummy+j));
+		}
+    }    
+    // Save all eigenvalues and a particular eigenvector
+    myParticle->eigvals.save("output_eigvals.txt",raw_ascii);
+    probDensity.save("output_state.txt", raw_ascii);
+    return 0;
+}
